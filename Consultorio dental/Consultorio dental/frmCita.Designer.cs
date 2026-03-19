69308bef461043b1d526737ba2fafefeb69b86f2
Alexander Acosta <alexarmonia1@gmail.com>
2026-03-18 21:55:16 -0400

    codificacion de tabla "Cita"

--- Consultorio dental/Consultorio dental/frmCita.Designer.cs
+++ Consultorio dental/Consultorio dental/frmCita.Designer.cs
@@ -28,12 +28,193 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "frmCita";
+            btnInsertar = new Button();
+            btnAtualizar = new Button();
+            btnEliminar = new Button();
+            btnExportar = new Button();
+            dgvCitas = new DataGridView();
+            btnExportarCsv = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            cmbPaciente = new ComboBox();
+            cmbDentista = new ComboBox();
+            cmbMotivo = new ComboBox();
+            dtpFecha = new DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)dgvCitas).BeginInit();
+            SuspendLayout();
+            // 
+            // btnInsertar
+            // 
+            btnInsertar.Location = new Point(359, 77);
+            btnInsertar.Name = "btnInsertar";
+            btnInsertar.Size = new Size(112, 34);
+            btnInsertar.TabIndex = 0;
+            btnInsertar.Text = "Insertar";
+            btnInsertar.UseVisualStyleBackColor = true;
+            btnInsertar.Click += btnInsertar_Click;
+            // 
+            // btnAtualizar
+            // 
+            btnAtualizar.Location = new Point(359, 117);
+            btnAtualizar.Name = "btnAtualizar";
+            btnAtualizar.Size = new Size(112, 34);
+            btnAtualizar.TabIndex = 1;
+            btnAtualizar.Text = "actualizar";
+            btnAtualizar.UseVisualStyleBackColor = true;
+            btnAtualizar.Click += btnAtualizar_Click;
+            // 
+            // btnEliminar
+            // 
+            btnEliminar.Location = new Point(497, 77);
+            btnEliminar.Name = "btnEliminar";
+            btnEliminar.Size = new Size(112, 34);
+            btnEliminar.TabIndex = 2;
+            btnEliminar.Text = "eliminar";
+            btnEliminar.UseVisualStyleBackColor = true;
+            btnEliminar.Click += btnEliminar_Click;
+            // 
+            // btnExportar
+            // 
+            btnExportar.Location = new Point(497, 117);
+            btnExportar.Name = "btnExportar";
+            btnExportar.Size = new Size(112, 34);
+            btnExportar.TabIndex = 3;
+            btnExportar.Text = "Exportar";
+            btnExportar.UseVisualStyleBackColor = true;
+            btnExportar.Click += btnExportar_Click;
+            // 
+            // dgvCitas
+            // 
+            dgvCitas.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvCitas.Location = new Point(12, 183);
+            dgvCitas.Name = "dgvCitas";
+            dgvCitas.RowHeadersWidth = 62;
+            dgvCitas.Size = new Size(925, 325);
+            dgvCitas.TabIndex = 4;
+            // 
+            // btnExportarCsv
+            // 
+            btnExportarCsv.Location = new Point(615, 100);
+            btnExportarCsv.Name = "btnExportarCsv";
+            btnExportarCsv.Size = new Size(140, 34);
+            btnExportarCsv.TabIndex = 3;
+            btnExportarCsv.Text = "Exportar csv";
+            btnExportarCsv.UseVisualStyleBackColor = true;
+            btnExportarCsv.Click += btnExportarCsv_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(28, 22);
+            label1.Name = "label1";
+            label1.Size = new Size(76, 25);
+            label1.TabIndex = 5;
+            label1.Text = "Paciente";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(28, 61);
+            label2.Name = "label2";
+            label2.Size = new Size(77, 25);
+            label2.TabIndex = 5;
+            label2.Text = "Dentista";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(35, 109);
+            label3.Name = "label3";
+            label3.Size = new Size(69, 25);
+            label3.TabIndex = 5;
+            label3.Text = "Motivo";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(359, 22);
+            label4.Name = "label4";
+            label4.Size = new Size(57, 25);
+            label4.TabIndex = 5;
+            label4.Text = "Fecha";
+            // 
+            // cmbPaciente
+            // 
+            cmbPaciente.FormattingEnabled = true;
+            cmbPaciente.Location = new Point(110, 19);
+            cmbPaciente.Name = "cmbPaciente";
+            cmbPaciente.Size = new Size(182, 33);
+            cmbPaciente.TabIndex = 6;
+            // 
+            // cmbDentista
+            // 
+            cmbDentista.FormattingEnabled = true;
+            cmbDentista.Location = new Point(110, 61);
+            cmbDentista.Name = "cmbDentista";
+            cmbDentista.Size = new Size(182, 33);
+            cmbDentista.TabIndex = 6;
+            // 
+            // cmbMotivo
+            // 
+            cmbMotivo.FormattingEnabled = true;
+            cmbMotivo.Location = new Point(110, 109);
+            cmbMotivo.Name = "cmbMotivo";
+            cmbMotivo.Size = new Size(182, 33);
+            cmbMotivo.TabIndex = 6;
+            // 
+            // dtpFecha
+            // 
+            dtpFecha.Format = DateTimePickerFormat.Short;
+            dtpFecha.Location = new Point(422, 22);
+            dtpFecha.Name = "dtpFecha";
+            dtpFecha.Size = new Size(156, 31);
+            dtpFecha.TabIndex = 7;
+            // 
+            // frmCita
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1169, 520);
+            Controls.Add(dtpFecha);
+            Controls.Add(cmbMotivo);
+            Controls.Add(cmbDentista);
+            Controls.Add(cmbPaciente);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(dgvCitas);
+            Controls.Add(btnExportarCsv);
+            Controls.Add(btnExportar);
+            Controls.Add(btnEliminar);
+            Controls.Add(btnAtualizar);
+            Controls.Add(btnInsertar);
+            Name = "frmCita";
+            Text = "frmCita";
+            Load += frmCita_Load;
+            ((System.ComponentModel.ISupportInitialize)dgvCitas).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button btnInsertar;
+        private Button btnAtualizar;
+        private Button btnEliminar;
+        private Button btnExportar;
+        private DataGridView dgvCitas;
+        private Button btnExportarCsv;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private ComboBox cmbPaciente;
+        private ComboBox cmbDentista;
+        private ComboBox cmbMotivo;
+        private DateTimePicker dtpFecha;
     }
 }
--- Consultorio dental/Consultorio dental/frmCita.cs
+++ Consultorio dental/Consultorio dental/frmCita.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Consultorio_dental.Models;
 
 namespace Consultorio_dental
 {
@@ -16,5 +17,321 @@ namespace Consultorio_dental
         {
             InitializeComponent();
         }
+
+        private void btnInsertar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                if (cmbPaciente.SelectedValue == null)
+                {
+                    MessageBox.Show("Debe seleccionar un paciente.");
+                    return;
+                }
+                if (cmbDentista.SelectedValue == null)
+                {
+                    MessageBox.Show("Debe seleccionar un dentista.");
+                    return;
+                }
+                if (cmbMotivo.SelectedValue == null)
+                {
+                    MessageBox.Show("Debe seleccionar un motivo.");
+                    return;
+                }
+
+                using var db = new ConsultorioContext();
+
+                var nuevaCita = new Citum
+                {
+                    PacienteId = (int)cmbPaciente.SelectedValue,
+                    DentistaId = (int)cmbDentista.SelectedValue,
+                    MotivoId = (int)cmbMotivo.SelectedValue,
+                    Fecha = DateOnly.FromDateTime(dtpFecha.Value)
+                };
+
+                db.Cita.Add(nuevaCita);
+                db.SaveChanges();
+
+                MessageBox.Show("Cita agregada correctamente");
+                CargarCitas();
+
+                // para Limpiar controles
+                cmbPaciente.SelectedIndex = -1;
+                cmbDentista.SelectedIndex = -1;
+                cmbMotivo.SelectedIndex = -1;
+                dtpFecha.Value = DateTime.Now;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al insertar cita: " + ex.Message);
+            }
+
+
+
+        }
+
+
+        private void CargarCitas()
+        {
+            using var db = new ConsultorioContext();
+
+            dgvCitas.DataSource = db.Cita
+                .Select(c => new
+                {
+                    c.CitaId,
+                    c.PacienteId,
+                    c.DentistaId,
+                    c.MotivoId,
+                    c.Fecha,
+                    Estado = c.Fecha.ToDateTime(TimeOnly.MinValue) > DateTime.Now ? "Pendiente" : "Vencida",
+                    TiempoRestante = c.Fecha.ToDateTime(TimeOnly.MinValue) > DateTime.Now
+                        ? (c.Fecha.ToDateTime(TimeOnly.MinValue) - DateTime.Now).Days + " días"
+                        : "0"
+                })
+                .ToList();
+        }
+
+        private void frmCita_Load(object sender, EventArgs e)
+        {
+            CargarCitas();
+            using var db = new ConsultorioContext();
+
+            // Pacientes
+            cmbPaciente.DataSource = db.Pacientes.ToList();
+            cmbPaciente.DisplayMember = "Nombre";      // lo que se ve en la lista
+            cmbPaciente.ValueMember = "PacienteId";    // el ID que se usa internamente
+            cmbPaciente.SelectedIndex = -1;            // para que arranque vacío
+
+            // Dentistas
+            cmbDentista.DataSource = db.Dentista.ToList();
+            cmbDentista.DisplayMember = "Nombre";
+            cmbDentista.ValueMember = "DentistaId";
+            cmbDentista.SelectedIndex = -1;
+
+            // Motivos
+            cmbMotivo.DataSource = db.Motivos.ToList();
+            cmbMotivo.DisplayMember = "Descripcion";   // o el campo que tengas
+            cmbMotivo.ValueMember = "MotivoId";
+            cmbMotivo.SelectedIndex = -1;
+
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAtualizar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Validar que haya una fila seleccionada
+                if (dgvCitas.CurrentRow == null)
+                {
+                    MessageBox.Show("Debe seleccionar una cita en la tabla.");
+                    return;
+                }
+
+                // Validar que los combos tengan valores
+                if (cmbPaciente.SelectedValue == null)
+                {
+                    MessageBox.Show("Debe seleccionar un paciente.");
+                    return;
+                }
+                if (cmbDentista.SelectedValue == null)
+                {
+                    MessageBox.Show("Debe seleccionar un dentista.");
+                    return;
+                }
+                if (cmbMotivo.SelectedValue == null)
+                {
+                    MessageBox.Show("Debe seleccionar un motivo.");
+                    return;
+                }
+
+                using var db = new ConsultorioContext();
+
+                int id = (int)dgvCitas.CurrentRow.Cells["CitaId"].Value;
+                var cita = db.Cita.Find(id);
+
+                if (cita != null)
+                {
+                    cita.PacienteId = (int)cmbPaciente.SelectedValue;
+                    cita.DentistaId = (int)cmbDentista.SelectedValue;
+                    cita.MotivoId = (int)cmbMotivo.SelectedValue;
+                    cita.Fecha = DateOnly.FromDateTime(dtpFecha.Value);
+
+                    db.SaveChanges();
+                    MessageBox.Show("Cita actualizada correctamente");
+                    CargarCitas();
+
+                    // Limpiar controles después de actualizar
+                    cmbPaciente.SelectedIndex = -1;
+                    cmbDentista.SelectedIndex = -1;
+                    cmbMotivo.SelectedIndex = -1;
+                    dtpFecha.Value = DateTime.Now;
+                }
+                else
+                {
+                    MessageBox.Show("No se encontró la cita seleccionada.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al actualizar cita: " + ex.Message);
+            }
+
+
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Validar que haya una fila seleccionada
+                if (dgvCitas.CurrentRow == null)
+                {
+                    MessageBox.Show("Debe seleccionar una cita en la tabla.");
+                    return;
+                }
+
+                using var db = new ConsultorioContext();
+
+                int id = (int)dgvCitas.CurrentRow.Cells["CitaId"].Value;
+                var cita = db.Cita.Find(id);
+
+                if (cita != null)
+                {
+                    // Confirmación antes de eliminar
+                    var confirmacion = MessageBox.Show(
+                        "¿Está seguro que desea eliminar esta cita?",
+                        "Confirmar eliminación",
+                        MessageBoxButtons.YesNo,
+                        MessageBoxIcon.Warning
+                    );
+
+                    if (confirmacion == DialogResult.Yes)
+                    {
+                        db.Cita.Remove(cita);
+                        db.SaveChanges();
+                        MessageBox.Show("Cita eliminada correctamente");
+                        CargarCitas();
+
+                        // 🔹 Limpiar controles después de eliminar
+                        cmbPaciente.SelectedIndex = -1;
+                        cmbDentista.SelectedIndex = -1;
+                        cmbMotivo.SelectedIndex = -1;
+                        dtpFecha.Value = DateTime.Now;
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("No se encontró la cita seleccionada.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al eliminar cita: " + ex.Message);
+            }
+
+
+        }
+
+        private void ExportarCitasCSV(string rutaArchivo)
+        {
+            using (var sw = new StreamWriter(rutaArchivo, false, Encoding.UTF8))
+            {
+                // Encabezados
+                var encabezados = string.Join(",", dgvCitas.Columns
+                    .Cast<DataGridViewColumn>()
+                    .Select(c => $"\"{c.HeaderText}\""));
+                sw.WriteLine(encabezados);
+
+                // Filas
+                foreach (DataGridViewRow fila in dgvCitas.Rows)
+                {
+                    if (!fila.IsNewRow)
+                    {
+                        var valores = string.Join(",", fila.Cells
+                            .Cast<DataGridViewCell>()
+                            .Select(c => $"\"{c.Value?.ToString()}\""));
+                        sw.WriteLine(valores);
+                    }
+                }
+            }
+
+            MessageBox.Show("Citas exportadas a CSV correctamente");
+        }
+
+        private void btnExportarCsv_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "CSV|*.csv";
+            saveFileDialog.Title = "Guardar listado de citas";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                ExportarCitasCSV(saveFileDialog.FileName);
+            }
+
+
+        }
+        private void ExportarCitasPDF(string rutaArchivo)
+        {
+            using (var fs = new FileStream(rutaArchivo, FileMode.Create, FileAccess.Write, FileShare.None))
+            using (var doc = new iTextSharp.text.Document())
+            using (var writer = iTextSharp.text.pdf.PdfWriter.GetInstance(doc, fs))
+            {
+                doc.Open();
+
+                // Título
+                doc.Add(new iTextSharp.text.Paragraph("Listado de Citas"));
+                doc.Add(new iTextSharp.text.Paragraph(" ")); // espacio
+
+                // Tabla PDF
+                var tabla = new iTextSharp.text.pdf.PdfPTable(dgvCitas.Columns.Count);
+
+                // Encabezados
+                foreach (DataGridViewColumn col in dgvCitas.Columns)
+                {
+                    tabla.AddCell(new iTextSharp.text.Phrase(col.HeaderText));
+                }
+
+                // Filas
+                foreach (DataGridViewRow fila in dgvCitas.Rows)
+                {
+                    if (!fila.IsNewRow)
+                    {
+                        foreach (DataGridViewCell celda in fila.Cells)
+                        {
+                            tabla.AddCell(new iTextSharp.text.Phrase(celda.Value?.ToString()));
+                        }
+                    }
+                }
+
+                doc.Add(tabla);
+                doc.Close();
+            }
+
+            MessageBox.Show("Citas exportadas a PDF correctamente");
+        }
+
+        private void btnExportar_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "PDF|*.pdf";
+            saveFileDialog.Title = "Guardar listado de citas";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                ExportarCitasPDF(saveFileDialog.FileName);
+            }
+
+
+        }
     }
+
+
 }

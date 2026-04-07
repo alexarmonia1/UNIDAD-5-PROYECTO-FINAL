e4497c8f7251597752cf29a134ef86ba03e0aba9
Alexander Acosta <alexarmonia1@gmail.com>
2026-04-06 23:06:22 -0400

    codificacion tabla "servicios" spa

--- Sistema de Citas para Spa/Sistema de Citas para Spa/frmServicios.Designer.cs
+++ Sistema de Citas para Spa/Sistema de Citas para Spa/frmServicios.Designer.cs
@@ -28,12 +28,171 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.btnInsertar = new System.Windows.Forms.Button();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnExportarPDF = new System.Windows.Forms.Button();
+            this.btnExportarCSV = new System.Windows.Forms.Button();
+            this.dgvServicios = new System.Windows.Forms.DataGridView();
+            this.numDuracion = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvServicios)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numDuracion)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(69, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(156, 42);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 26);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(69, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Duracion";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(69, 142);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 20);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Precio";
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(156, 142);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(100, 26);
+            this.txtPrecio.TabIndex = 1;
+            // 
+            // btnInsertar
+            // 
+            this.btnInsertar.Location = new System.Drawing.Point(45, 220);
+            this.btnInsertar.Name = "btnInsertar";
+            this.btnInsertar.Size = new System.Drawing.Size(88, 32);
+            this.btnInsertar.TabIndex = 2;
+            this.btnInsertar.Text = "Insertar";
+            this.btnInsertar.UseVisualStyleBackColor = true;
+            this.btnInsertar.Click += new System.EventHandler(this.btnInsertar_Click);
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.Location = new System.Drawing.Point(156, 220);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(88, 32);
+            this.btnActualizar.TabIndex = 2;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(250, 220);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(88, 32);
+            this.btnEliminar.TabIndex = 2;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnExportarPDF
+            // 
+            this.btnExportarPDF.Location = new System.Drawing.Point(353, 220);
+            this.btnExportarPDF.Name = "btnExportarPDF";
+            this.btnExportarPDF.Size = new System.Drawing.Size(119, 32);
+            this.btnExportarPDF.TabIndex = 2;
+            this.btnExportarPDF.Text = "ExportarPDF";
+            this.btnExportarPDF.UseVisualStyleBackColor = true;
+            this.btnExportarPDF.Click += new System.EventHandler(this.btnExportarPDF_Click);
+            // 
+            // btnExportarCSV
+            // 
+            this.btnExportarCSV.Location = new System.Drawing.Point(496, 220);
+            this.btnExportarCSV.Name = "btnExportarCSV";
+            this.btnExportarCSV.Size = new System.Drawing.Size(122, 32);
+            this.btnExportarCSV.TabIndex = 2;
+            this.btnExportarCSV.Text = "ExportarCSV";
+            this.btnExportarCSV.UseVisualStyleBackColor = true;
+            this.btnExportarCSV.Click += new System.EventHandler(this.btnExportarCSV_Click);
+            // 
+            // dgvServicios
+            // 
+            this.dgvServicios.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvServicios.Location = new System.Drawing.Point(45, 280);
+            this.dgvServicios.Name = "dgvServicios";
+            this.dgvServicios.RowHeadersWidth = 62;
+            this.dgvServicios.RowTemplate.Height = 28;
+            this.dgvServicios.Size = new System.Drawing.Size(758, 340);
+            this.dgvServicios.TabIndex = 3;
+            this.dgvServicios.SelectionChanged += new System.EventHandler(this.dgvServicios_SelectionChanged);
+            // 
+            // numDuracion
+            // 
+            this.numDuracion.Location = new System.Drawing.Point(156, 87);
+            this.numDuracion.Name = "numDuracion";
+            this.numDuracion.Size = new System.Drawing.Size(120, 26);
+            this.numDuracion.TabIndex = 4;
+            // 
+            // frmServicios
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(873, 677);
+            this.Controls.Add(this.numDuracion);
+            this.Controls.Add(this.dgvServicios);
+            this.Controls.Add(this.btnExportarCSV);
+            this.Controls.Add(this.btnExportarPDF);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnActualizar);
+            this.Controls.Add(this.btnInsertar);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmServicios";
             this.Text = "frmServicios";
+            this.Load += new System.EventHandler(this.frmServicios_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvServicios)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numDuracion)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.Button btnInsertar;
+        private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnExportarPDF;
+        private System.Windows.Forms.Button btnExportarCSV;
+        private System.Windows.Forms.DataGridView dgvServicios;
+        private System.Windows.Forms.NumericUpDown numDuracion;
     }
 }
--- Sistema de Citas para Spa/Sistema de Citas para Spa/frmServicios.cs
+++ Sistema de Citas para Spa/Sistema de Citas para Spa/frmServicios.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using Sistema_de_Citas_para_Spa.modelos;
 
 namespace Sistema_de_Citas_para_Spa
 {
@@ -15,6 +19,290 @@ namespace Sistema_de_Citas_para_Spa
         public frmServicios()
         {
             InitializeComponent();
+        }
+
+        private void cargarServicios()
+        {
+            using (var db = new spaEntities())
+            {
+                var servicios = db.Servicios
+                    .Select(s => new
+                    {
+                        s.ServicioId,
+                        s.Nombre,
+                        s.DuracionMinutos,
+                        s.Precio
+                    })
+                    .ToList();
+
+                dgvServicios.DataSource = servicios;
+            }
+        }
+
+        private void limpiarCampos()
+        {
+            txtNombre.Clear();
+            txtPrecio.Clear();
+            
+        }
+
+        private void frmServicios_Load(object sender, EventArgs e)
+        {
+            cargarServicios();
+        }
+
+        private void btnInsertar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Validar campos vacíos
+                if (string.IsNullOrWhiteSpace(txtNombre.Text) ||
+                    string.IsNullOrWhiteSpace(txtPrecio.Text))
+                {
+                    MessageBox.Show("Todos los campos son obligatorios.");
+                    limpiarCampos();
+                    return;
+                }
+
+                // Validar que Precio sea decimal
+                if (!decimal.TryParse(txtPrecio.Text, out decimal precio))
+                {
+                    MessageBox.Show("El precio debe ser un número válido.");
+                    return;
+                }
+
+                using (var db = new spaEntities())
+                {
+                    var nuevo = new Servicios
+                    {
+                        Nombre = txtNombre.Text,
+                        DuracionMinutos = (int)numDuracion.Value, 
+                        Precio = precio
+                    };
+
+                    db.Servicios.Add(nuevo);
+                    db.SaveChanges();
+                }
+
+                MessageBox.Show("Servicio agregado correctamente.");
+                cargarServicios();
+                limpiarCampos();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al insertar servicio: " + ex.Message);
+            }
+
+
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                if (string.IsNullOrWhiteSpace(txtNombre.Text) ||
+                    string.IsNullOrWhiteSpace(txtPrecio.Text))
+                {
+                    MessageBox.Show("Todos los campos son obligatorios.");
+                    return;
+                }
+
+                
+                if (!decimal.TryParse(txtPrecio.Text, out decimal precio))
+                {
+                    MessageBox.Show("El precio debe ser un número válido.");
+                    return;
+                }
+
+                
+                if (dgvServicios.CurrentRow == null)
+                {
+                    MessageBox.Show("Seleccione un servicio de la lista.");
+                    return;
+                }
+
+                int id = Convert.ToInt32(dgvServicios.CurrentRow.Cells["ServicioId"].Value);
+
+                using (var db = new spaEntities())
+                {
+                    var servicio = db.Servicios.FirstOrDefault(s => s.ServicioId == id);
+
+                    if (servicio != null)
+                    {
+                        servicio.Nombre = txtNombre.Text;
+                        servicio.DuracionMinutos = (int)numDuracion.Value;
+                        servicio.Precio = precio;
+
+                        db.SaveChanges();
+                        MessageBox.Show("Servicio actualizado correctamente.");
+                        limpiarCampos();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Servicio no encontrado.");
+                    }
+                }
+
+                cargarServicios(); // refresca el DataGridView
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al actualizar servicio: " + ex.Message);
+            }
+
+
+        }
+
+        private void dgvServicios_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dgvServicios.CurrentRow != null)
+            {
+                // Nombre
+                txtNombre.Text = dgvServicios.CurrentRow.Cells["Nombre"].Value.ToString();
+
+                // Duración (NumericUpDown)
+                if (int.TryParse(dgvServicios.CurrentRow.Cells["DuracionMinutos"].Value.ToString(), out int duracion))
+                {
+                    numDuracion.Value = duracion;
+                }
+
+                // Precio
+                txtPrecio.Text = dgvServicios.CurrentRow.Cells["Precio"].Value.ToString();
+            }
+
+
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                if (dgvServicios.CurrentRow == null)
+                {
+                    MessageBox.Show("Seleccione un servicio de la lista.");
+                    return;
+                }
+
+                int id = Convert.ToInt32(dgvServicios.CurrentRow.Cells["ServicioId"].Value);
+
+                using (var db = new spaEntities())
+                {
+                    var servicio = db.Servicios.FirstOrDefault(s => s.ServicioId == id);
+
+                    if (servicio != null)
+                    {
+                        db.Servicios.Remove(servicio);
+                        db.SaveChanges();
+                        MessageBox.Show("Servicio eliminado correctamente.");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Servicio no encontrado.");
+                    }
+                }
+
+                cargarServicios(); 
+                limpiarCampos();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al eliminar servicio: " + ex.Message);
+            }
+
+
+        }
+
+        private void btnExportarPDF_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "PDF files (*.pdf)|*.pdf";
+                saveFileDialog.Title = "Guardar servicios en PDF";
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string ruta = saveFileDialog.FileName;
+
+                    Document doc = new Document(PageSize.A4);
+                    PdfWriter.GetInstance(doc, new FileStream(ruta, FileMode.Create));
+                    doc.Open();
+
+                    doc.Add(new Paragraph("Lista de Servicios"));
+                    doc.Add(new Paragraph(" "));
+
+                    PdfPTable tabla = new PdfPTable(4);
+                    tabla.AddCell("ID");
+                    tabla.AddCell("Nombre");
+                    tabla.AddCell("Duración (min)");
+                    tabla.AddCell("Precio");
+
+                    foreach (DataGridViewRow fila in dgvServicios.Rows)
+                    {
+                        if (fila.Cells["ServicioId"].Value != null)
+                        {
+                            tabla.AddCell(fila.Cells["ServicioId"].Value.ToString());
+                            tabla.AddCell(fila.Cells["Nombre"].Value.ToString());
+                            tabla.AddCell(fila.Cells["DuracionMinutos"].Value.ToString());
+                            tabla.AddCell(fila.Cells["Precio"].Value.ToString());
+                        }
+                    }
+
+                    doc.Add(tabla);
+                    doc.Close();
+
+                    MessageBox.Show("PDF de servicios generado correctamente en: " + ruta);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al exportar PDF: " + ex.Message);
+            }
+
+
+        }
+
+        private void btnExportarCSV_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "CSV files (*.csv)|*.csv";
+                saveFileDialog.Title = "Guardar servicios en CSV";
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string ruta = saveFileDialog.FileName;
+
+                    using (StreamWriter sw = new StreamWriter(ruta))
+                    {
+                        sw.WriteLine("ServicioId,Nombre,DuracionMinutos,Precio");
+
+                        foreach (DataGridViewRow fila in dgvServicios.Rows)
+                        {
+                            if (fila.Cells["ServicioId"].Value != null)
+                            {
+                                string linea = fila.Cells["ServicioId"].Value.ToString() + "," +
+                                               fila.Cells["Nombre"].Value.ToString() + "," +
+                                               fila.Cells["DuracionMinutos"].Value.ToString() + "," +
+                                               fila.Cells["Precio"].Value.ToString();
+
+                                sw.WriteLine(linea);
+                            }
+                        }
+                    }
+
+                    MessageBox.Show("CSV de servicios generado correctamente en: " + ruta);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al exportar CSV: " + ex.Message);
+            }
+
+
         }
     }
 }

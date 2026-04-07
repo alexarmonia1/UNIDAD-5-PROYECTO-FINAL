f07329f134f7e8013da95951549f01a5779c7340
Alexander Acosta <alexarmonia1@gmail.com>
2026-04-06 23:25:50 -0400

    codificacion tabla "terapeutas" spa

--- Sistema de Citas para Spa/Sistema de Citas para Spa/frmTerapeutas.Designer.cs
+++ Sistema de Citas para Spa/Sistema de Citas para Spa/frmTerapeutas.Designer.cs
@@ -28,12 +28,147 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtEspecialidad = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.btnInsertar = new System.Windows.Forms.Button();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnExportarPDF = new System.Windows.Forms.Button();
+            this.btnExportarCSV = new System.Windows.Forms.Button();
+            this.dgvTerapeutas = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTerapeutas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 128);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Especialidad";
+            // 
+            // txtEspecialidad
+            // 
+            this.txtEspecialidad.Location = new System.Drawing.Point(117, 128);
+            this.txtEspecialidad.Name = "txtEspecialidad";
+            this.txtEspecialidad.Size = new System.Drawing.Size(166, 26);
+            this.txtEspecialidad.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Nombre";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(117, 71);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(166, 26);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // btnInsertar
+            // 
+            this.btnInsertar.Location = new System.Drawing.Point(16, 203);
+            this.btnInsertar.Name = "btnInsertar";
+            this.btnInsertar.Size = new System.Drawing.Size(82, 40);
+            this.btnInsertar.TabIndex = 2;
+            this.btnInsertar.Text = "Insertar";
+            this.btnInsertar.UseVisualStyleBackColor = true;
+            this.btnInsertar.Click += new System.EventHandler(this.btnInsertar_Click);
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.Location = new System.Drawing.Point(104, 203);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(102, 40);
+            this.btnActualizar.TabIndex = 3;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(212, 203);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(84, 40);
+            this.btnEliminar.TabIndex = 3;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnExportarPDF
+            // 
+            this.btnExportarPDF.Location = new System.Drawing.Point(16, 256);
+            this.btnExportarPDF.Name = "btnExportarPDF";
+            this.btnExportarPDF.Size = new System.Drawing.Size(119, 33);
+            this.btnExportarPDF.TabIndex = 3;
+            this.btnExportarPDF.Text = "ExportarPDF";
+            this.btnExportarPDF.UseVisualStyleBackColor = true;
+            this.btnExportarPDF.Click += new System.EventHandler(this.btnExportarPDF_Click);
+            // 
+            // btnExportarCSV
+            // 
+            this.btnExportarCSV.Location = new System.Drawing.Point(151, 256);
+            this.btnExportarCSV.Name = "btnExportarCSV";
+            this.btnExportarCSV.Size = new System.Drawing.Size(120, 33);
+            this.btnExportarCSV.TabIndex = 3;
+            this.btnExportarCSV.Text = "ExportarCSV";
+            this.btnExportarCSV.UseVisualStyleBackColor = true;
+            this.btnExportarCSV.Click += new System.EventHandler(this.btnExportarCSV_Click);
+            // 
+            // dgvTerapeutas
+            // 
+            this.dgvTerapeutas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTerapeutas.Location = new System.Drawing.Point(345, 12);
+            this.dgvTerapeutas.Name = "dgvTerapeutas";
+            this.dgvTerapeutas.RowHeadersWidth = 62;
+            this.dgvTerapeutas.RowTemplate.Height = 28;
+            this.dgvTerapeutas.Size = new System.Drawing.Size(636, 426);
+            this.dgvTerapeutas.TabIndex = 4;
+            this.dgvTerapeutas.SelectionChanged += new System.EventHandler(this.dgvTerapeutas_SelectionChanged);
+            // 
+            // frmTerapeutas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(988, 450);
+            this.Controls.Add(this.dgvTerapeutas);
+            this.Controls.Add(this.btnExportarCSV);
+            this.Controls.Add(this.btnExportarPDF);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnActualizar);
+            this.Controls.Add(this.btnInsertar);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtEspecialidad);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmTerapeutas";
             this.Text = "frmTerapeutas";
+            this.Load += new System.EventHandler(this.frmTerapeutas_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTerapeutas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtEspecialidad;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Button btnInsertar;
+        private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnExportarPDF;
+        private System.Windows.Forms.Button btnExportarCSV;
+        private System.Windows.Forms.DataGridView dgvTerapeutas;
     }
 }
--- Sistema de Citas para Spa/Sistema de Citas para Spa/frmTerapeutas.cs
+++ Sistema de Citas para Spa/Sistema de Citas para Spa/frmTerapeutas.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using Sistema_de_Citas_para_Spa.modelos;
 
 namespace Sistema_de_Citas_para_Spa
 {
@@ -15,6 +19,258 @@ namespace Sistema_de_Citas_para_Spa
         public frmTerapeutas()
         {
             InitializeComponent();
+        }
+
+        private void cargarTerapeutas()
+        {
+            using (var db = new spaEntities())
+            {
+                var terapeutas = db.Terapeutas
+                    .Select(t => new
+                    {
+                        t.TerapeutaId,
+                        t.Nombre,
+                        t.Especialidad
+                    })
+                    .ToList();
+
+                dgvTerapeutas.DataSource = terapeutas;
+            }
+        }
+
+        private void limpiarCampos()
+        {
+            txtNombre.Clear();
+            txtEspecialidad.Clear();
+        }
+
+        
+
+        private void btnInsertar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(txtNombre.Text) ||
+                    string.IsNullOrWhiteSpace(txtEspecialidad.Text))
+                {
+                    MessageBox.Show("Todos los campos son obligatorios.");
+                    txtNombre.Focus();
+                    return;
+                }
+
+                using (var db = new spaEntities())
+                {
+                    var nuevo = new Terapeutas
+                    {
+                        Nombre = txtNombre.Text,
+                        Especialidad = txtEspecialidad.Text
+                    };
+
+                    db.Terapeutas.Add(nuevo);
+                    db.SaveChanges();
+                }
+
+                MessageBox.Show("Terapeuta agregado correctamente.");
+                cargarTerapeutas();
+                limpiarCampos();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al insertar terapeuta: " + ex.Message);
+            }
+
+
+        }
+
+        private void frmTerapeutas_Load(object sender, EventArgs e)
+        {
+            cargarTerapeutas();
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(txtNombre.Text) ||
+                    string.IsNullOrWhiteSpace(txtEspecialidad.Text))
+                {
+                    MessageBox.Show("Todos los campos son obligatorios.");
+                    return;
+                }
+
+                if (dgvTerapeutas.CurrentRow == null)
+                {
+                    MessageBox.Show("Seleccione un terapeuta de la lista.");
+                    return;
+                }
+
+                int id = Convert.ToInt32(dgvTerapeutas.CurrentRow.Cells["TerapeutaId"].Value);
+
+                using (var db = new spaEntities())
+                {
+                    var terapeuta = db.Terapeutas.FirstOrDefault(t => t.TerapeutaId == id);
+
+                    if (terapeuta != null)
+                    {
+                        terapeuta.Nombre = txtNombre.Text;
+                        terapeuta.Especialidad = txtEspecialidad.Text;
+
+                        db.SaveChanges();
+                        MessageBox.Show("Terapeuta actualizado correctamente.");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Terapeuta no encontrado.");
+                    }
+                }
+
+                cargarTerapeutas();
+                limpiarCampos();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al actualizar terapeuta: " + ex.Message);
+            }
+
+
+        }
+
+        private void dgvTerapeutas_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dgvTerapeutas.CurrentRow != null)
+            {
+                txtNombre.Text = dgvTerapeutas.CurrentRow.Cells["Nombre"].Value.ToString();
+                txtEspecialidad.Text = dgvTerapeutas.CurrentRow.Cells["Especialidad"].Value.ToString();
+            }
+
+
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (dgvTerapeutas.CurrentRow == null)
+                {
+                    MessageBox.Show("Seleccione un terapeuta de la lista.");
+                    return;
+                }
+
+                int id = Convert.ToInt32(dgvTerapeutas.CurrentRow.Cells["TerapeutaId"].Value);
+
+                using (var db = new spaEntities())
+                {
+                    var terapeuta = db.Terapeutas.FirstOrDefault(t => t.TerapeutaId == id);
+
+                    if (terapeuta != null)
+                    {
+                        db.Terapeutas.Remove(terapeuta);
+                        db.SaveChanges();
+                        MessageBox.Show("Terapeuta eliminado correctamente.");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Terapeuta no encontrado.");
+                    }
+                }
+
+                cargarTerapeutas();
+                limpiarCampos();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al eliminar terapeuta: " + ex.Message);
+            }
+
+
+        }
+
+        private void btnExportarPDF_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "PDF files (*.pdf)|*.pdf";
+                saveFileDialog.Title = "Guardar terapeutas en PDF";
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string ruta = saveFileDialog.FileName;
+
+                    Document doc = new Document(PageSize.A4);
+                    PdfWriter.GetInstance(doc, new FileStream(ruta, FileMode.Create));
+                    doc.Open();
+
+                    doc.Add(new Paragraph("Lista de Terapeutas"));
+                    doc.Add(new Paragraph(" "));
+
+                    PdfPTable tabla = new PdfPTable(3);
+                    tabla.AddCell("ID");
+                    tabla.AddCell("Nombre");
+                    tabla.AddCell("Especialidad");
+
+                    foreach (DataGridViewRow fila in dgvTerapeutas.Rows)
+                    {
+                        if (fila.Cells["TerapeutaId"].Value != null)
+                        {
+                            tabla.AddCell(fila.Cells["TerapeutaId"].Value.ToString());
+                            tabla.AddCell(fila.Cells["Nombre"].Value.ToString());
+                            tabla.AddCell(fila.Cells["Especialidad"].Value.ToString());
+                        }
+                    }
+
+                    doc.Add(tabla);
+                    doc.Close();
+
+                    MessageBox.Show("PDF de terapeutas generado correctamente en: " + ruta);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al exportar PDF: " + ex.Message);
+            }
+
+
+        }
+
+        private void btnExportarCSV_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "CSV files (*.csv)|*.csv";
+                saveFileDialog.Title = "Guardar terapeutas en CSV";
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string ruta = saveFileDialog.FileName;
+
+                    using (StreamWriter sw = new StreamWriter(ruta))
+                    {
+                        sw.WriteLine("TerapeutaId,Nombre,Especialidad");
+
+                        foreach (DataGridViewRow fila in dgvTerapeutas.Rows)
+                        {
+                            if (fila.Cells["TerapeutaId"].Value != null)
+                            {
+                                string linea = fila.Cells["TerapeutaId"].Value.ToString() + "," +
+                                               fila.Cells["Nombre"].Value.ToString() + "," +
+                                               fila.Cells["Especialidad"].Value.ToString();
+
+                                sw.WriteLine(linea);
+                            }
+                        }
+                    }
+
+                    MessageBox.Show("CSV de terapeutas generado correctamente en: " + ruta);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al exportar CSV: " + ex.Message);
+            }
+
+
         }
     }
 }

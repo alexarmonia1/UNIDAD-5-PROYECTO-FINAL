7731f78a0a5c8483d0a60bfa252bf23352f6f71e
Alexander Acosta <alexarmonia1@gmail.com>
2026-04-08 22:58:53 -0400

    codificacion tabla "materias"

--- Sistema de Calificacion de Estudiantes/Sistema de Calificacion de Estudiantes/frmMaterias.Designer.cs
+++ Sistema de Calificacion de Estudiantes/Sistema de Calificacion de Estudiantes/frmMaterias.Designer.cs
@@ -29,11 +29,158 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.numCreditos = new System.Windows.Forms.NumericUpDown();
+            this.btnInsertar = new System.Windows.Forms.Button();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnExportarPDF = new System.Windows.Forms.Button();
+            this.btnExportarCSV = new System.Windows.Forms.Button();
+            this.dgvMaterias = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.numCreditos)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMaterias)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(-3, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre de Materia";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(147, 61);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(120, 26);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(73, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Creditos";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // numCreditos
+            // 
+            this.numCreditos.Location = new System.Drawing.Point(147, 112);
+            this.numCreditos.Name = "numCreditos";
+            this.numCreditos.Size = new System.Drawing.Size(120, 26);
+            this.numCreditos.TabIndex = 3;
+            // 
+            // btnInsertar
+            // 
+            this.btnInsertar.Location = new System.Drawing.Point(36, 194);
+            this.btnInsertar.Name = "btnInsertar";
+            this.btnInsertar.Size = new System.Drawing.Size(75, 31);
+            this.btnInsertar.TabIndex = 4;
+            this.btnInsertar.Text = "Insertar";
+            this.btnInsertar.UseVisualStyleBackColor = true;
+            this.btnInsertar.Click += new System.EventHandler(this.btnInsertar_Click);
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.Location = new System.Drawing.Point(117, 194);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(112, 31);
+            this.btnActualizar.TabIndex = 4;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(36, 231);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 31);
+            this.btnEliminar.TabIndex = 4;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnExportarPDF
+            // 
+            this.btnExportarPDF.Location = new System.Drawing.Point(36, 277);
+            this.btnExportarPDF.Name = "btnExportarPDF";
+            this.btnExportarPDF.Size = new System.Drawing.Size(124, 31);
+            this.btnExportarPDF.TabIndex = 4;
+            this.btnExportarPDF.Text = "ExportarPDF";
+            this.btnExportarPDF.UseVisualStyleBackColor = true;
+            this.btnExportarPDF.Click += new System.EventHandler(this.btnExportarPDF_Click);
+            // 
+            // btnExportarCSV
+            // 
+            this.btnExportarCSV.Location = new System.Drawing.Point(36, 314);
+            this.btnExportarCSV.Name = "btnExportarCSV";
+            this.btnExportarCSV.Size = new System.Drawing.Size(124, 31);
+            this.btnExportarCSV.TabIndex = 4;
+            this.btnExportarCSV.Text = "ExportarCSV";
+            this.btnExportarCSV.UseVisualStyleBackColor = true;
+            this.btnExportarCSV.Click += new System.EventHandler(this.btnExportarCSV_Click);
+            // 
+            // dgvMaterias
+            // 
+            this.dgvMaterias.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMaterias.Location = new System.Drawing.Point(297, 12);
+            this.dgvMaterias.Name = "dgvMaterias";
+            this.dgvMaterias.RowHeadersWidth = 62;
+            this.dgvMaterias.RowTemplate.Height = 28;
+            this.dgvMaterias.Size = new System.Drawing.Size(491, 408);
+            this.dgvMaterias.TabIndex = 5;
+            this.dgvMaterias.SelectionChanged += new System.EventHandler(this.dgvMaterias_SelectionChanged);
+            // 
+            // frmMaterias
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvMaterias);
+            this.Controls.Add(this.btnExportarCSV);
+            this.Controls.Add(this.btnExportarPDF);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnActualizar);
+            this.Controls.Add(this.btnInsertar);
+            this.Controls.Add(this.numCreditos);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmMaterias";
             this.Text = "frmMaterias";
+            this.Load += new System.EventHandler(this.frmMaterias_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numCreditos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMaterias)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numCreditos;
+        private System.Windows.Forms.Button btnInsertar;
+        private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnExportarPDF;
+        private System.Windows.Forms.Button btnExportarCSV;
+        private System.Windows.Forms.DataGridView dgvMaterias;
     }
 }
--- Sistema de Calificacion de Estudiantes/Sistema de Calificacion de Estudiantes/frmMaterias.cs
+++ Sistema de Calificacion de Estudiantes/Sistema de Calificacion de Estudiantes/frmMaterias.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using Sistema_de_Calificacion_de_Estudiantes.Modelos;
 
 namespace Sistema_de_Calificacion_de_Estudiantes
 {
@@ -15,6 +19,260 @@ namespace Sistema_de_Calificacion_de_Estudiantes
         public frmMaterias()
         {
             InitializeComponent();
+        }
+        private void cargarMaterias()
+        {
+            using (var db = new estudiantesEntities())
+            {
+                var materias = db.Materias
+                    .Select(m => new
+                    {
+                        m.MateriaId,
+                        m.Nombre,
+                        m.Creditos
+                    })
+                    .ToList();
+
+                dgvMaterias.DataSource = materias;
+            }
+        }
+
+        private void limpiarCampos()
+        {
+            txtNombre.Clear();
+            numCreditos.Value = 0;
+           
+        }
+
+     
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmMaterias_Load(object sender, EventArgs e)
+        {
+            cargarMaterias();
+        }
+
+        private void btnInsertar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(txtNombre.Text))
+                {
+                    MessageBox.Show("El nombre es obligatorio.");
+                    return;
+                }
+
+                using (var db = new estudiantesEntities())
+                {
+                    var nueva = new Materias
+                    {
+                        Nombre = txtNombre.Text,
+                        Creditos = (int)numCreditos.Value
+                    };
+
+                    db.Materias.Add(nueva);
+                    db.SaveChanges();
+                }
+
+                MessageBox.Show("Materia agregada correctamente.");
+                cargarMaterias();
+                limpiarCampos();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al insertar materia: " + ex.Message);
+            }
+
+
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(txtNombre.Text))
+                {
+                    MessageBox.Show("El nombre es obligatorio.");
+                    return;
+                }
+
+                if (dgvMaterias.CurrentRow == null)
+                {
+                    MessageBox.Show("Seleccione una materia de la lista.");
+                    return;
+                }
+
+                int id = Convert.ToInt32(dgvMaterias.CurrentRow.Cells["MateriaId"].Value);
+
+                using (var db = new estudiantesEntities())
+                {
+                    var materia = db.Materias.FirstOrDefault(m => m.MateriaId == id);
+
+                    if (materia != null)
+                    {
+                        materia.Nombre = txtNombre.Text;
+                        materia.Creditos = (int)numCreditos.Value;
+
+                        db.SaveChanges();
+                        MessageBox.Show("Materia actualizada correctamente.");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Materia no encontrada.");
+                    }
+                }
+
+                cargarMaterias();
+                limpiarCampos();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al actualizar materia: " + ex.Message);
+            }
+
+
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (dgvMaterias.CurrentRow == null)
+                {
+                    MessageBox.Show("Seleccione una materia de la lista.");
+                    return;
+                }
+
+                int id = Convert.ToInt32(dgvMaterias.CurrentRow.Cells["MateriaId"].Value);
+
+                using (var db = new estudiantesEntities())
+                {
+                    var materia = db.Materias.FirstOrDefault(m => m.MateriaId == id);
+
+                    if (materia != null)
+                    {
+                        db.Materias.Remove(materia);
+                        db.SaveChanges();
+                        MessageBox.Show("Materia eliminada correctamente.");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Materia no encontrada.");
+                    }
+                }
+
+                cargarMaterias();
+                limpiarCampos() ;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al eliminar materia: " + ex.Message);
+            }
+
+
+        }
+
+        private void btnExportarPDF_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "PDF files (*.pdf)|*.pdf";
+                saveFileDialog.Title = "Guardar materias en PDF";
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string ruta = saveFileDialog.FileName;
+
+                    Document doc = new Document(PageSize.A4);
+                    PdfWriter.GetInstance(doc, new FileStream(ruta, FileMode.Create));
+                    doc.Open();
+
+                    doc.Add(new Paragraph("Lista de Materias"));
+                    doc.Add(new Paragraph(" "));
+
+                    PdfPTable tabla = new PdfPTable(3);
+                    tabla.AddCell("ID");
+                    tabla.AddCell("Nombre");
+                    tabla.AddCell("Créditos");
+
+                    foreach (DataGridViewRow fila in dgvMaterias.Rows)
+                    {
+                        if (fila.Cells["MateriaId"].Value != null)
+                        {
+                            tabla.AddCell(fila.Cells["MateriaId"].Value.ToString());
+                            tabla.AddCell(fila.Cells["Nombre"].Value.ToString());
+                            tabla.AddCell(fila.Cells["Creditos"].Value.ToString());
+                        }
+                    }
+
+                    doc.Add(tabla);
+                    doc.Close();
+
+                    MessageBox.Show("PDF de materias generado correctamente en: " + ruta);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al exportar PDF: " + ex.Message);
+            }
+
+
+        }
+
+        private void btnExportarCSV_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "CSV files (*.csv)|*.csv";
+                saveFileDialog.Title = "Guardar materias en CSV";
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string ruta = saveFileDialog.FileName;
+
+                    using (StreamWriter sw = new StreamWriter(ruta))
+                    {
+                        sw.WriteLine("MateriaId,Nombre,Creditos");
+
+                        foreach (DataGridViewRow fila in dgvMaterias.Rows)
+                        {
+                            if (fila.Cells["MateriaId"].Value != null)
+                            {
+                                string linea = fila.Cells["MateriaId"].Value.ToString() + "," +
+                                               fila.Cells["Nombre"].Value.ToString() + "," +
+                                               fila.Cells["Creditos"].Value.ToString();
+
+                                sw.WriteLine(linea);
+                            }
+                        }
+                    }
+
+                    MessageBox.Show("CSV de materias generado correctamente en: " + ruta);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al exportar CSV: " + ex.Message);
+            }
+
+
+        }
+
+        private void dgvMaterias_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dgvMaterias.CurrentRow != null)
+            {
+                txtNombre.Text = dgvMaterias.CurrentRow.Cells["Nombre"].Value.ToString();
+                numCreditos.Value = Convert.ToInt32(dgvMaterias.CurrentRow.Cells["Creditos"].Value);
+            }
+
+
         }
     }
 }

414529b5b933e137daf34a08ad9051c7da78faa0
Alexander Acosta <alexarmonia1@gmail.com>
2026-04-09 22:46:15 -0400

    codificacion tabla "cargos"

---
+++ sistema de manejo de empleados/sistema de manejo de empleados/cargos.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace sistema_de_manejo_de_empleados
+{
+    partial class cargos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvCargos = new System.Windows.Forms.DataGridView();
+            this.btnExportarCSV = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnExportarPDF = new System.Windows.Forms.Button();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnInsertar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCargos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvCargos
+            // 
+            this.dgvCargos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCargos.Location = new System.Drawing.Point(382, 12);
+            this.dgvCargos.Name = "dgvCargos";
+            this.dgvCargos.RowHeadersWidth = 62;
+            this.dgvCargos.RowTemplate.Height = 28;
+            this.dgvCargos.Size = new System.Drawing.Size(490, 454);
+            this.dgvCargos.TabIndex = 10;
+            this.dgvCargos.SelectionChanged += new System.EventHandler(this.dgvCargos_SelectionChanged);
+            // 
+            // btnExportarCSV
+            // 
+            this.btnExportarCSV.Location = new System.Drawing.Point(156, 199);
+            this.btnExportarCSV.Name = "btnExportarCSV";
+            this.btnExportarCSV.Size = new System.Drawing.Size(116, 39);
+            this.btnExportarCSV.TabIndex = 6;
+            this.btnExportarCSV.Text = "ExportarCSV";
+            this.btnExportarCSV.UseVisualStyleBackColor = true;
+            this.btnExportarCSV.Click += new System.EventHandler(this.btnExportarCSV_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(213, 142);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(108, 39);
+            this.btnEliminar.TabIndex = 7;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnExportarPDF
+            // 
+            this.btnExportarPDF.Location = new System.Drawing.Point(21, 199);
+            this.btnExportarPDF.Name = "btnExportarPDF";
+            this.btnExportarPDF.Size = new System.Drawing.Size(129, 39);
+            this.btnExportarPDF.TabIndex = 8;
+            this.btnExportarPDF.Text = "ExportarPDF";
+            this.btnExportarPDF.UseVisualStyleBackColor = true;
+            this.btnExportarPDF.Click += new System.EventHandler(this.btnExportarPDF_Click);
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.Location = new System.Drawing.Point(109, 142);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(98, 39);
+            this.btnActualizar.TabIndex = 9;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(91, 73);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(181, 26);
+            this.txtNombre.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Cargo";
+            // 
+            // btnInsertar
+            // 
+            this.btnInsertar.Location = new System.Drawing.Point(28, 142);
+            this.btnInsertar.Name = "btnInsertar";
+            this.btnInsertar.Size = new System.Drawing.Size(75, 39);
+            this.btnInsertar.TabIndex = 11;
+            this.btnInsertar.Text = "Insertar";
+            this.btnInsertar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnInsertar.UseVisualStyleBackColor = true;
+            this.btnInsertar.Click += new System.EventHandler(this.btnInsertar_Click);
+            // 
+            // cargos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(894, 474);
+            this.Controls.Add(this.btnInsertar);
+            this.Controls.Add(this.dgvCargos);
+            this.Controls.Add(this.btnExportarCSV);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnExportarPDF);
+            this.Controls.Add(this.btnActualizar);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label1);
+            this.Name = "cargos";
+            this.Text = "cargos";
+            this.Load += new System.EventHandler(this.cargos_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCargos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvCargos;
+        private System.Windows.Forms.Button btnExportarCSV;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnExportarPDF;
+        private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnInsertar;
+    }
+}
---
+++ sistema de manejo de empleados/sistema de manejo de empleados/cargos.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using sistema_de_manejo_de_empleados.modelos;
+
+namespace sistema_de_manejo_de_empleados
+{
+    public partial class cargos : Form
+    {
+        public cargos()
+        {
+            InitializeComponent();
+        }
+
+        private void cargarCargos()
+        {
+            using (var db = new empleadosEntities())
+            {
+                var cargos = db.Cargos
+                    .Select(c => new
+                    {
+                        c.CargoId,
+                        c.Nombre
+                    })
+                    .ToList();
+
+                dgvCargos.DataSource = cargos;
+            }
+        }
+
+        private void limpiarCampos()
+        {
+            txtNombre.Clear();
+        }
+
+        private void cargos_Load(object sender, EventArgs e)
+        {
+            cargarCargos();
+        }
+
+        private void btnInsertar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(txtNombre.Text))
+                {
+                    MessageBox.Show("El nombre es obligatorio.");
+                    return;
+                }
+
+                using (var db = new empleadosEntities())
+                {
+                    var nuevo = new Cargos
+                    {
+                        Nombre = txtNombre.Text
+                    };
+
+                    db.Cargos.Add(nuevo);
+                    db.SaveChanges();
+                }
+
+                MessageBox.Show("Cargo agregado correctamente.");
+                cargarCargos();
+                limpiarCampos();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al insertar cargo: " + ex.Message);
+            }
+
+
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(txtNombre.Text))
+                {
+                    MessageBox.Show("El nombre es obligatorio.");
+                    return;
+                }
+
+                if (dgvCargos.CurrentRow == null)
+                {
+                    MessageBox.Show("Seleccione un cargo de la lista.");
+                    return;
+                }
+
+                int id = Convert.ToInt32(dgvCargos.CurrentRow.Cells["CargoId"].Value);
+
+                using (var db = new empleadosEntities())
+                {
+                    var cargo = db.Cargos.FirstOrDefault(c => c.CargoId == id);
+
+                    if (cargo != null)
+                    {
+                        cargo.Nombre = txtNombre.Text;
+                        db.SaveChanges();
+                        MessageBox.Show("Cargo actualizado correctamente.");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Cargo no encontrado.");
+                    }
+                }
+
+                cargarCargos();
+                limpiarCampos();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al actualizar cargo: " + ex.Message);
+            }
+
+
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (dgvCargos.CurrentRow == null)
+                {
+                    MessageBox.Show("Seleccione un cargo de la lista.");
+                    return;
+                }
+
+                int id = Convert.ToInt32(dgvCargos.CurrentRow.Cells["CargoId"].Value);
+
+                using (var db = new empleadosEntities())
+                {
+                    var cargo = db.Cargos.FirstOrDefault(c => c.CargoId == id);
+
+                    if (cargo != null)
+                    {
+                        db.Cargos.Remove(cargo);
+                        db.SaveChanges();
+                        MessageBox.Show("Cargo eliminado correctamente.");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Cargo no encontrado.");
+                    }
+                }
+
+                cargarCargos();
+                limpiarCampos();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al eliminar cargo: " + ex.Message);
+            }
+
+
+        }
+
+        private void btnExportarPDF_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "PDF files (*.pdf)|*.pdf";
+            saveFileDialog.Title = "Guardar cargos en PDF";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string ruta = saveFileDialog.FileName;
+
+                Document doc = new Document(PageSize.A4);
+                PdfWriter.GetInstance(doc, new FileStream(ruta, FileMode.Create));
+                doc.Open();
+
+                doc.Add(new Paragraph("Lista de Cargos"));
+                doc.Add(new Paragraph(" "));
+
+                PdfPTable tabla = new PdfPTable(2);
+                tabla.AddCell("ID");
+                tabla.AddCell("Nombre");
+
+                foreach (DataGridViewRow fila in dgvCargos.Rows)
+                {
+                    if (fila.Cells["CargoId"].Value != null)
+                    {
+                        tabla.AddCell(fila.Cells["CargoId"].Value.ToString());
+                        tabla.AddCell(fila.Cells["Nombre"].Value.ToString());
+                    }
+                }
+
+                doc.Add(tabla);
+                doc.Close();
+
+                MessageBox.Show("PDF generado correctamente en: " + ruta);
+            }
+
+
+        }
+
+        private void btnExportarCSV_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "CSV files (*.csv)|*.csv";
+            saveFileDialog.Title = "Guardar cargos en CSV";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string ruta = saveFileDialog.FileName;
+
+                using (StreamWriter sw = new StreamWriter(ruta))
+                {
+                    sw.WriteLine("CargoId,Nombre");
+
+                    foreach (DataGridViewRow fila in dgvCargos.Rows)
+                    {
+                        if (fila.Cells["CargoId"].Value != null)
+                        {
+                            string linea =
+                                fila.Cells["CargoId"].Value.ToString() + "," +
+                                fila.Cells["Nombre"].Value.ToString();
+
+                            sw.WriteLine(linea);
+                        }
+                    }
+                }
+
+                MessageBox.Show("CSV generado correctamente en: " + ruta);
+            }
+
+
+        }
+
+        private void dgvCargos_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dgvCargos.CurrentRow != null)
+            {
+                
+                txtNombre.Text = dgvCargos.CurrentRow.Cells["Nombre"].Value.ToString();
+            }
+
+
+        }
+    }
+}

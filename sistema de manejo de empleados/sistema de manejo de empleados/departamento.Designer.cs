c9dd4658613bb0dfaa0756687fd9c4af0a14fb35
Alexander Acosta <alexarmonia1@gmail.com>
2026-04-09 22:35:14 -0400

    codificacion tabla "departamentos

---
+++ sistema de manejo de empleados/sistema de manejo de empleados/departamento.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace sistema_de_manejo_de_empleados
+{
+    partial class departamento
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.btnInsertar = new System.Windows.Forms.Button();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.btnExportarPDF = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnExportarCSV = new System.Windows.Forms.Button();
+            this.dgvDepartamentos = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDepartamentos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(172, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre Departamento";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(181, 86);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(181, 26);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // btnInsertar
+            // 
+            this.btnInsertar.Location = new System.Drawing.Point(12, 155);
+            this.btnInsertar.Name = "btnInsertar";
+            this.btnInsertar.Size = new System.Drawing.Size(75, 39);
+            this.btnInsertar.TabIndex = 2;
+            this.btnInsertar.Text = "Insertar";
+            this.btnInsertar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnInsertar.UseVisualStyleBackColor = true;
+            this.btnInsertar.Click += new System.EventHandler(this.btnInsertar_Click);
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.Location = new System.Drawing.Point(100, 155);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(98, 39);
+            this.btnActualizar.TabIndex = 2;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
+            // 
+            // btnExportarPDF
+            // 
+            this.btnExportarPDF.Location = new System.Drawing.Point(12, 212);
+            this.btnExportarPDF.Name = "btnExportarPDF";
+            this.btnExportarPDF.Size = new System.Drawing.Size(129, 39);
+            this.btnExportarPDF.TabIndex = 2;
+            this.btnExportarPDF.Text = "ExportarPDF";
+            this.btnExportarPDF.UseVisualStyleBackColor = true;
+            this.btnExportarPDF.Click += new System.EventHandler(this.btnExportarPDF_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(204, 155);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(108, 39);
+            this.btnEliminar.TabIndex = 2;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnExportarCSV
+            // 
+            this.btnExportarCSV.Location = new System.Drawing.Point(147, 212);
+            this.btnExportarCSV.Name = "btnExportarCSV";
+            this.btnExportarCSV.Size = new System.Drawing.Size(116, 39);
+            this.btnExportarCSV.TabIndex = 2;
+            this.btnExportarCSV.Text = "ExportarCSV";
+            this.btnExportarCSV.UseVisualStyleBackColor = true;
+            this.btnExportarCSV.Click += new System.EventHandler(this.btnExportarCSV_Click);
+            // 
+            // dgvDepartamentos
+            // 
+            this.dgvDepartamentos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDepartamentos.Location = new System.Drawing.Point(381, 12);
+            this.dgvDepartamentos.Name = "dgvDepartamentos";
+            this.dgvDepartamentos.RowHeadersWidth = 62;
+            this.dgvDepartamentos.RowTemplate.Height = 28;
+            this.dgvDepartamentos.Size = new System.Drawing.Size(490, 454);
+            this.dgvDepartamentos.TabIndex = 3;
+            this.dgvDepartamentos.SelectionChanged += new System.EventHandler(this.dgvDepartamentos_SelectionChanged);
+            // 
+            // departamento
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(894, 494);
+            this.Controls.Add(this.dgvDepartamentos);
+            this.Controls.Add(this.btnExportarCSV);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnExportarPDF);
+            this.Controls.Add(this.btnActualizar);
+            this.Controls.Add(this.btnInsertar);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label1);
+            this.Name = "departamento";
+            this.Text = "departamento";
+            this.Load += new System.EventHandler(this.departamento_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDepartamentos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Button btnInsertar;
+        private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.Button btnExportarPDF;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnExportarCSV;
+        private System.Windows.Forms.DataGridView dgvDepartamentos;
+    }
+}
---
+++ sistema de manejo de empleados/sistema de manejo de empleados/departamento.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using sistema_de_manejo_de_empleados.modelos;
+
+namespace sistema_de_manejo_de_empleados
+{
+    public partial class departamento : Form
+    {
+        public departamento()
+        {
+            InitializeComponent();
+        }
+
+        private void cargarDepartamentos()
+        {
+            using (var db = new empleadosEntities())
+            {
+                var departamentos = db.Departamentos
+                    .Select(d => new
+                    {
+                        d.DepartamentoId,
+                        d.Nombre
+                    })
+                    .ToList();
+
+                dgvDepartamentos.DataSource = departamentos;
+            }
+        }
+
+        private void limpiarCampos()
+        {
+            txtNombre.Clear();
+        }
+
+        private void departamento_Load(object sender, EventArgs e)
+        {
+            cargarDepartamentos();
+        }
+
+        private void btnInsertar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(txtNombre.Text))
+                {
+                    MessageBox.Show("El nombre es obligatorio.");
+                    return;
+                }
+
+                using (var db = new empleadosEntities())
+                {
+                    var nuevo = new Departamentos
+                    {
+                        Nombre = txtNombre.Text
+                    };
+
+                    db.Departamentos.Add(nuevo);
+                    db.SaveChanges();
+                }
+
+                MessageBox.Show("Departamento agregado correctamente.");
+                cargarDepartamentos();
+                limpiarCampos();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al insertar departamento: " + ex.Message);
+            }
+
+
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(txtNombre.Text))
+                {
+                    MessageBox.Show("El nombre es obligatorio.");
+                    return;
+                }
+
+                if (dgvDepartamentos.CurrentRow == null)
+                {
+                    MessageBox.Show("Seleccione un departamento de la lista.");
+                    return;
+                }
+
+                int id = Convert.ToInt32(dgvDepartamentos.CurrentRow.Cells["DepartamentoId"].Value);
+
+                using (var db = new empleadosEntities())
+                {
+                    var departamento = db.Departamentos.FirstOrDefault(d => d.DepartamentoId == id);
+
+                    if (departamento != null)
+                    {
+                        departamento.Nombre = txtNombre.Text;
+                        db.SaveChanges();
+                        MessageBox.Show("Departamento actualizado correctamente.");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Departamento no encontrado.");
+                    }
+                }
+
+                cargarDepartamentos();
+                limpiarCampos();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al actualizar departamento: " + ex.Message);
+            }
+
+
+        }
+
+        private void dgvDepartamentos_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dgvDepartamentos.CurrentRow != null)
+            {
+                txtNombre.Text = dgvDepartamentos.CurrentRow.Cells["Nombre"].Value.ToString();
+            }
+
+
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (dgvDepartamentos.CurrentRow == null)
+                {
+                    MessageBox.Show("Seleccione un departamento de la lista.");
+                    return;
+                }
+
+                int id = Convert.ToInt32(dgvDepartamentos.CurrentRow.Cells["DepartamentoId"].Value);
+
+                using (var db = new empleadosEntities())
+                {
+                    var departamento = db.Departamentos.FirstOrDefault(d => d.DepartamentoId == id);
+
+                    if (departamento != null)
+                    {
+                        db.Departamentos.Remove(departamento);
+                        db.SaveChanges();
+                        MessageBox.Show("Departamento eliminado correctamente.");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Departamento no encontrado.");
+                    }
+                }
+
+                cargarDepartamentos();
+                limpiarCampos();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al eliminar departamento: " + ex.Message);
+            }
+
+
+        }
+
+        private void btnExportarPDF_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "PDF files (*.pdf)|*.pdf";
+                saveFileDialog.Title = "Guardar departamentos en PDF";
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string ruta = saveFileDialog.FileName;
+
+                    Document doc = new Document(PageSize.A4);
+                    PdfWriter.GetInstance(doc, new FileStream(ruta, FileMode.Create));
+                    doc.Open();
+
+                    doc.Add(new Paragraph("Lista de Departamentos"));
+                    doc.Add(new Paragraph(" "));
+
+                    PdfPTable tabla = new PdfPTable(2); // ID y Nombre
+                    tabla.AddCell("ID");
+                    tabla.AddCell("Nombre");
+
+                    foreach (DataGridViewRow fila in dgvDepartamentos.Rows)
+                    {
+                        if (fila.Cells["DepartamentoId"].Value != null)
+                        {
+                            tabla.AddCell(fila.Cells["DepartamentoId"].Value.ToString());
+                            tabla.AddCell(fila.Cells["Nombre"].Value.ToString());
+                        }
+                    }
+
+                    doc.Add(tabla);
+                    doc.Close();
+
+                    MessageBox.Show("PDF generado correctamente en: " + ruta);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al exportar PDF: " + ex.Message);
+            }
+
+
+        }
+
+        private void btnExportarCSV_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "CSV files (*.csv)|*.csv";
+                saveFileDialog.Title = "Guardar departamentos en CSV";
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string ruta = saveFileDialog.FileName;
+
+                    using (StreamWriter sw = new StreamWriter(ruta))
+                    {
+                        sw.WriteLine("DepartamentoId,Nombre");
+
+                        foreach (DataGridViewRow fila in dgvDepartamentos.Rows)
+                        {
+                            if (fila.Cells["DepartamentoId"].Value != null)
+                            {
+                                string linea =
+                                    fila.Cells["DepartamentoId"].Value.ToString() + "," +
+                                    fila.Cells["Nombre"].Value.ToString();
+
+                                sw.WriteLine(linea);
+                            }
+                        }
+                    }
+
+                    MessageBox.Show("CSV generado correctamente en: " + ruta);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al exportar CSV: " + ex.Message);
+            }
+
+
+        }
+    }
+
+
+
+
+
+}
